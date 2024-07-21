414e123770dd2e423681b74e83f8bf3fc6586879
alfonsoOramasJr <alfonso.oramas@icloud.com>
2024-07-21 13:15:52 -0400

    FIX: Truncated the data to fit our models.

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ from database.database_management import DatabaseManager
 import random
 
 # Constants
-INPUT_SIZE = 100  # Number of nodes in each input layer
+INPUT_SIZE = 1000  # Number of nodes in each input layer
 HIDDEN_LAYER_NODES = 64  # Number of nodes in the hidden layers
 
 def create_dual_channel_model():
@@ -99,9 +99,19 @@ def load_data_from_db():
 
     # Separate the combined data into x1, x2, and y
     channel1_data, channel2_data, labels = zip(*combined_data)
-    x1 = np.array(channel1_data).reshape(-1, INPUT_SIZE)
-    x2 = np.array(channel2_data).reshape(-1, INPUT_SIZE)
-    y = tf.keras.utils.to_categorical(labels, num_classes=5)
+    channel1_data = np.array(channel1_data)
+    channel2_data = np.array(channel2_data)
+
+    # Ensure the total number of data points is a multiple of INPUT_SIZE
+    num_samples = len(channel1_data) - (len(channel1_data) % INPUT_SIZE)
+    channel1_data = channel1_data[:num_samples]
+    channel2_data = channel2_data[:num_samples]
+    labels = labels[:num_samples]
+
+    # Reshape the data
+    x1 = channel1_data.reshape(-1, INPUT_SIZE)
+    x2 = channel2_data.reshape(-1, INPUT_SIZE)
+    y = tf.keras.utils.to_categorical(labels[:len(x1)], num_classes=5)
 
     return x1, x2, y
 
@@ -123,8 +133,8 @@ if __name__ == "__main__":
     isolated_channel_single_output_model.fit([x1, x2], y, epochs=10, batch_size=32)
     
     # Save the models
-    dual_channel_model.save('dual_channel_model.h5')
-    isolated_channel_single_output_model.save('isolated_channel_single_output_model.h5')
+    # dual_channel_model.save('dual_channel_model.h5')
+    # isolated_channel_single_output_model.save('isolated_channel_single_output_model.h5')
     
     # To load the models
     # loaded_dual_channel_model = tf.keras.models.load_model('dual_channel_model.h5')
